0df7a665e3169c30622b8958959b44c636914f12
valuvilla <valuvp@hotmail.com>
2023-03-02 10:01:07 +0100

    cambios

--- teoría.py
+++ teoría.py
@@ -9,36 +9,34 @@ class Vehiculo():
         return f"Color: {self.color}, {self.ruedas} ruedas"
 
 class Coche(Vehiculo):
-    def __init__(self, color, ruedas, velocidad, cilindrada,tipo_vehiculo):
-        super().__init__(color, ruedas, tipo_vehiculo)
-        self.tipo_vehiculo="Coche"
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        super().__init__(color, ruedas)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
 
     def __str__(self):
-       return Vehiculo.__str__(self) + f", Velocidad:  {self.velocidad} km/h, Cilindrada: {self.cilindrada} cc, Tipo de vehiculo: {self.tipo_vehiculo}"
+       return Vehiculo.__str__(self) + f", Velocidad:  {self.velocidad} km/h, Cilindrada: {self.cilindrada} cc"
 
     
 class Bicicleta(Vehiculo):
-    def __init__(self, color, ruedas, tipo, tipo_vehiculo):
+    def __init__(self, color, ruedas, tipo):
         super().__init__(color, ruedas)
-        self.tipo_vehiculo="Bicicleta"
         self.tipo=tipo
 
     def __str__(self):
-       return Vehiculo.__str__(self) + f", tipo: {self.tipo}, Tipo de vehiculo: {self.tipo_vehiculo}"
+       return Vehiculo.__str__(self) + f", tipo: {self.tipo}"
 
 class Camioneta(Coche):
-    def __init__(self, color, ruedas, velocidad, cilindrada, carga, tipo_vehiculo):
-        super().__init__(color, ruedas, velocidad, cilindrada, tipo_vehiculo)
+    def __init__(self, color, ruedas, velocidad, cilindrada, carga):
+        super().__init__(color, ruedas, velocidad, cilindrada)
         self.carga=carga
 
     def __str__(self):
          return Coche.__str__(self) + f",Carga: {self.carga} kg de carga"
     
 class Motocicleta(Bicicleta):
-    def __init__(self, color, ruedas, tipo, velocidad, cilindrada, tipo_vehiculo):
-        super().__init__(color, ruedas, tipo, tipo_vehiculo)
+    def __init__(self, color, ruedas, tipo, velocidad, cilindrada):
+        super().__init__(color, ruedas, tipo)
         self.velocidad=velocidad
         self.cilindrada=cilindrada
 
@@ -50,38 +48,44 @@ class Motocicleta(Bicicleta):
 
 class Vehiculos():
 
-    lista=[]
+    lista_coche=[]
+    lista_bicicleta=[]
     with open(config.DATABASE_PATH, "r") as file:
-        reader=csv.reader(file, delimiter=";")
-        for color, ruedas, velocidad, cilindrada, tipo, carga, tipo_vehiculo in reader:
-            if tipo_vehiculo=="Coche":
-                if carga==None:
-                    vehiculo=Coche(color, ruedas, velocidad, cilindrada,tipo_vehiculo)
-                vehiculo=Camioneta(color, ruedas, velocidad, cilindrada,carga)
-            elif tipo_vehiculo=="Bicicleta":
-                if velocidad==None:
-                    vehiculo=Bicicleta(color, ruedas, tipo, tipo_vehiculo)
-                vehiculo=Motocicleta(color, ruedas, tipo, velocidad, cilindrada)
-            lista.append(vehiculo)
+
 
     
 
     @staticmethod
-    def agregar_lista(color, ruedas, velocidad, cilindrada,tipo,carga,tipo_vehiculo):
-        # Definir el tipo de vehiculo
-        if tipo_vehiculo=="Coche":
-            if carga==None:
-                vehiculo=Coche(color, ruedas, velocidad, cilindrada,tipo_vehiculo)
-            vehiculo=Camioneta(color, ruedas, velocidad, cilindrada,carga)
-        elif tipo_vehiculo=="Bicicleta":
-            if velocidad==None:
-                vehiculo=Bicicleta(color, ruedas, tipo, tipo_vehiculo)
-            vehiculo=Motocicleta(color, ruedas, tipo, velocidad, cilindrada)
-        Vehiculos.lista.append(vehiculo)
-        return vehiculo
+    def buscar_vehiculo(ruedas):
+        for vehiculo in Vehiculos.lista_coche:
+            if vehiculo.ruedas == ruedas:
+                return vehiculo
+        for vehiculo in Vehiculos.lista_bicicleta:
+            if vehiculo.ruedas == ruedas:
+                return vehiculo
+        return None
     
-
-
+    @staticmethod
+    def agregar_lista(color, ruedas, velocidad, cilindrada, tipo, carga):
+        if ruedas == 4:
+            if carga == None:
+                coche=Coche(color, ruedas, velocidad, cilindrada)
+                Vehiculos.lista_coche.append(coche)
+                return coche
+            else:
+                camioneta=Camioneta(color, ruedas, velocidad, cilindrada, carga)
+                Vehiculos.lista_coche.append(camioneta)
+                return camioneta
+        elif ruedas == 2:
+            if tipo == "urbana":
+                bicicleta=Bicicleta(color, ruedas, tipo)
+                Vehiculos.lista_bicicleta.append(bicicleta)
+            elif tipo == "deportiva":
+                motocicleta=Motocicleta(color, ruedas, tipo, velocidad, cilindrada)
+                Vehiculos.lista_bicicleta.append(motocicleta)
+        
+        
+    
     
 
 
